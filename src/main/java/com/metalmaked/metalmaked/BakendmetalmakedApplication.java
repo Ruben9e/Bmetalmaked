cc861b596bc1e3eaed9fd0ac08072264bc741a23
Sena <dimatesebas73@gmail.com>
2025-02-27 18:29:23 -0500

    create proyect

---
+++ src/main/java/com/metalmaked/metalmaked/BakendmetalmakedApplication.java
@@ -0,0 +1,13 @@
+package com.metalmaked.metalmaked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendmetalmakedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendmetalmakedApplication.class, args);
+	}
+
+}
